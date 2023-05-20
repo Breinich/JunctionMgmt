1350dfe5a05bd9c920591e550c7154a9020292a4
Breinich <bajnokvencel@edu.bme.hu>
2023-05-21 01:44:52 +0200

    junction environment and framework initial

--- junctionMgmt/src/java/JunctionEnv.java
+++ junctionMgmt/src/java/JunctionEnv.java
@@ -1,14 +1,21 @@
 import java.util.logging.Logger;
 
 import jason.environment.Environment;
+import world.Simulator;
 
 
 public class JunctionEnv extends Environment{
 	
 	private Logger logger = Logger.getLogger("junctionMgmt."+JunctionEnv.class.getName());
+	private Simulator simulator;
     
     
 	public JunctionEnv() {
 		
 	}
+	
+	@Override
+    public void init(String[] args) {
+        super.init(args);
+    }
 }
