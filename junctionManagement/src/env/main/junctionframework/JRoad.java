0062b27ce084a5e9bfbaae7a0d1690eb53b49dc1
Breinich <bajnokvencel@edu.bme.hu>
2023-05-29 07:22:26 +0200

    cleanup

--- junctionManagement/src/env/main/junctionframework/JRoad.java
+++ junctionManagement/src/env/main/junctionframework/JRoad.java
@@ -1,26 +1,21 @@
 package main.junctionframework;
 
-import main.world.Direction;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.LineNumberReader;
 import java.util.LinkedList;
 
 public class JRoad extends JPanel {
 
-    private LinkedList<Coordinate> vehiclesCoordinates;
-    private String path;
+    private final LinkedList<Coordinate> vehiclesCoordinates;
 
-    BufferedImage image;
+    private BufferedImage image;
 
     public JRoad(String path) {
         super(true);
-        this.path = path;
 
         try {
             this.image = ImageIO.read(new File(path));
@@ -39,8 +34,6 @@ public class JRoad extends JPanel {
     public void removeAllCoordinate() {
     	vehiclesCoordinates.clear();
     }
-
-
 
     @Override
     public void paintComponent(Graphics g) {
